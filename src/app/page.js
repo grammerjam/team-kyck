015d38f94cde7a951f9d3ff6aaef6afe86af228c
sause668 <soulsitescoding@zohomail.com>
2025-04-17 16:55:59 -0400

    login edit

--- src/app/page.js
+++ src/app/page.js
@@ -3,24 +3,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      {/* <div id="loginImgCon">
-        <img src="\assets\logo.svg" alt="KyckFlix logo." />
-      </div>
-      <div id="loginInfoCon">
-        <h1 id="loginTitle">
-          Login<br/>
-        </h1>
+        <div id="loginImgCon">
+            <img src="/assets/logo.svg" alt="KyckFlix logo." />
+        </div>
+        {/* <div style="background-color: #161D2F; border-radius:6%; width:400px; height:373px; margin: auto; font-family: sans-serif; color:white; padding: auto; text-align: center;">
+            <h1 style="font-weight: lighter; margin-bottom: 40px; float: left; margin-left: 24px;">
+                Login<br/>
+            </h1>
 
-        <input id="loginInputEmail" type="email" placeholder="   Email address"/>
-        <br/>
-        <input id="loginInputPassword" type="password" placeholder="   Password"/>
-        <br/>
-        <button id="loginButtonSubmit">Login to your account</button>
-        <br/>
-        <p id="loginSignUpCon">
-          Don't have an account? <a id="loginSignUpLink" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Sign Up</a>
-        </p>
-      </div> */}
+            <input type="email" placeholder="   Email address" style="border: 0px none transparent; border-bottom: 1px solid #5A698F ; background-color: transparent; color:white; margin-bottom:24px; width: 336px; height: 37px;"/>
+            <br/>
+            <input type="password" placeholder="   Password" style="border: 0px none transparent; border-bottom: 1px solid #5A698F ; background-color: transparent; color:white; margin-bottom:40px; width: 336px; height: 37px;"/>
+            <br/>
+            <button style=" background-color: #FC4747; color:white; border:0px none transparent; border-radius: 6px; width: 336px; height: 48px; margin-bottom:12px;">Login to your account</button>
+            <br/>
+            <p style="text-align: center;">
+                Don't have an account? <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" style="color: #FC4747; font-weight: lighter; text-decoration: none;">Sign Up</a>
+            </p>
+        </div> */}
     </div>
   );
 }
