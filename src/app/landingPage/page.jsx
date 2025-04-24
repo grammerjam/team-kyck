af6f3b1938c1832d3f2a64ae248359cb499dac94
Caspian Tyler <TreadTheDawnGames>
2025-04-23 22:38:26 -0400

    fixed typo Kick -> Kyck

--- src/app/landingPage/page.jsx
+++ src/app/landingPage/page.jsx
@@ -2,25 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div style="background: url(./LandingPageBackground.png) no-repeat center center; height: 100vh; 
-        background-size:cover; margin-left:0; 
-        margin-right:0;">
-            <div style="border:solid white 2px; background:#0a0a0a; justify-self: center; color:white; font-family: sans-serif; position:relative; top:38%;">
-                <div style="margin:30px; ">
+    <div id="backgroundImage">
+            <div id="marginMaker" >
+                <div id="margin30px">
 
-                    <h1>Do a KickFlip!</h1>
-                    <div style="justify-self: center;">
-                        <span style="margin-right:100px;">
-                            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" style="color: #FC4747; font-weight: lighter; text-decoration: none;">Sign In</a>
+                    <h1>Do a KyckFlip!</h1>
+                    <div id="justifySelfCenter">
+                        <span id="leftSideSignButton">
+                            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" id="signInButtonColor">Sign In</a>
                         </span>
                         <span>
-                            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" style="color: #FC4747; font-weight: lighter; text-decoration: none;">Sign Up</a>
+                            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" id="signInButtonColor">Sign Up</a>
                         </span>
                     </div>
                 </div>
             </div>
-            <div style="border:solid white 2px; background:#0a0a0a; justify-self: center; color:white; font-family: sans-serif; position:relative; top:40%;">
-                <div style="margin:20px; ">
+            <div id="lowerBox">
+                <div id="margin20px">
                     This site doesn't do much yet, but it will. Team Kyck is on the job!
                 </div>
             </div>
