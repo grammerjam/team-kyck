b322cbd8e347872ea04686a9a5b1d0021fb3ac8c
sause668 <soulsitescoding@zohomail.com>
2025-05-07 17:28:57 -0400

    removed redux from frontend

--- P1/Website/frontend/src/App.jsx
+++ P1/Website/frontend/src/App.jsx
@@ -1,26 +1,14 @@
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import * as sessionActions from './store/session';
 import HomePage from './components/HomePage/HomePage';
 import LoginPage from './components/LoginPage/LoginPage';
 import LandingPage from './components/LandingPage/LandingPage';
 import SignPage from './components/SignupPage/SignupPage';
 
 function Layout() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => {
-      setIsLoaded(true)
-    });
-  }, [dispatch]);
 
   return (
     <>
-      {/* <Navigation isLoaded={isLoaded} /> */}
-      {isLoaded && <Outlet />}
+      <Outlet />
     </>
   );
 }
