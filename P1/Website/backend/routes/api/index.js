42d76aa0105a4cb11d607f5aeb55c2fd7265d43a
sause668 <soulsitescoding@zohomail.com>
2025-05-10 12:04:53 -0400

    set user routes

--- P1/Website/backend/routes/api/index.js
+++ P1/Website/backend/routes/api/index.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const spotsRouter = require('./spots.js');
-const bookingsRouter = require('./bookings.js');
-const reviewsRouter = require('./reviews.js');
-const reviewImagesRouter = require('./review-images.js');
-const spotImagesRouter = require('./spot-images.js');
 
 const { restoreUser } = require("../../utils/auth.js");
 
@@ -17,14 +12,5 @@ router.use(restoreUser);
 //Set Routers
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
-router.use('/spots', spotsRouter);
-router.use('/bookings', bookingsRouter);
-router.use('/reviews', reviewsRouter);
-router.use('/review-images', reviewImagesRouter);
-router.use('/spot-images', spotImagesRouter);
-
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
 
 module.exports = router;
