30052beb6524928ab194160556d0f0ac42721434
sause668 <soulsitescoding@zohomail.com>
2025-05-12 18:41:58 -0400

    users

--- P1/Website/backend/db/migrations/20240715225112-create-user.js
+++ P1/Website/backend/db/migrations/20240715225112-create-user.js
@@ -35,7 +35,7 @@ module.exports = {
         unique: true
       },
       hashedPassword: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING.BINARY,
         allowNull: false
       },
       createdAt: {
