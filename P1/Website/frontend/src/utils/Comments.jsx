9de696e0849018f7bf73c09af837f189fbfa11e5
Sparerow1 <yc01jun6@gmail.com>
2025-06-01 22:37:20 -0400

    editing star

--- P1/Website/frontend/src/utils/Comments.jsx
+++ P1/Website/frontend/src/utils/Comments.jsx
@@ -74,8 +74,8 @@ export function Comments(){
                 <div>
                     <img src="assets/image-avatar.png" alt="Avatar" className={styles.avatar} />
                     <span className={styles.commentingUser}>Your Name</span>
-                    <InteractiveStarRating />
                 </div>
+                <InteractiveStarRating className={styles.interactiveStar}/>
                 <div className={styles.commentInputContainer}>
                     <textarea 
                         ref={textareaRef}
