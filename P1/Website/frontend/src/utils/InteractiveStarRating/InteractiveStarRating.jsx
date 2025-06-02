67999b3655c22492e03c8920964898d5c4fda989
Sparerow1 <yc01jun6@gmail.com>
2025-06-01 22:18:31 -0400

    editing star feature

--- P1/Website/frontend/src/utils/InteractiveStarRating/InteractiveStarRating.jsx
+++ P1/Website/frontend/src/utils/InteractiveStarRating/InteractiveStarRating.jsx
@@ -4,7 +4,7 @@ import styles from './InteractiveStarRating.module.css';
 export function InteractiveStarRating() {
     const [rating, setRating] = useState(0);
     const [hoveredRating, setHoveredRating] = useState(0);
-    const stars = [];
+    const stars = []; // Array to hold star elements
     const maxRating = 5;
 
     for (let i = 1; i <= maxRating; i++) {
@@ -23,7 +23,6 @@ export function InteractiveStarRating() {
     }
     return(
             <div className={styles.interactiveStarContainer}>
-                <span className={styles.ratingLabel}>Rate: </span>
                 <div className={styles.starRating}>{stars}</div>
                 {rating > 0 && (
                     <span className={styles.selectedRating}>({rating} star{rating !== 1 ? 's' : ''})</span>
